a2ee5b400ade280ede050e369894e1be373f094f
Andre Zumaeta <andrezumaeta299@.com>
2025-09-27 00:38:24 +0200

    feat: Se agregaron variables db_user, db_password, db_name para la conexion con aurora

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,3 +1,21 @@
 variable "aws_region" { default = "us-east-1" }
 variable "project" { default = "mvp-lms" }
 variable "vpc_id" { default = "" } 
+
+variable "db_user" {
+  type        = string
+  description = "Aurora master username"
+  default     = "admin"
+}
+
+variable "db_password" {
+  type        = string
+  description = "Aurora master password"
+  sensitive   = true
+}
+
+variable "db_name" {
+  type        = string
+  description = "Aurora database name"
+  default     = "lmsdb"
+}
